e2ef18d574cd6104ce47f91a038003a2f17b3bbc
Daron976 <daronkwiafe@gmail.com>
2023-01-24 22:39:37 +0200

    Implement usable wireframe

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,10 +5,10 @@ class PostsController < ApplicationController
     @user = [User.find(@author_id)]
     id_array = []
     @comments = []
-    @posts = Post.where(:AuthorId == @author_id)
+    @posts = Post.where(@author_id == :AuthorId)
     @posts.each { |post| id_array.push(post.id) }
     id_array.each do |el|
-      @comments.push(Comment.where(:PostId == el))
+      @comments.push(Comment.where(el == :PostId))
     end
   end
 
@@ -17,6 +17,6 @@ class PostsController < ApplicationController
     @post_id = params[:post_id].to_i
     @user = User.find(@author_id)
     @post = Post.find(@post_id)
-    @post_comments = Comment.where(:PostId == @post_id)
+    @post_comments = Comment.where(@post_id == :PostId)
   end
 end
